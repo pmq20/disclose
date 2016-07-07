223fb8082a928478d8bf728043f3bfda8156f323
Minqi Pan <pmq2001@gmail.com>
2016-07-07 09:59:55 +0800

    Add argv passing

--- lib/disclose/c.rb
+++ lib/disclose/c.rb
@@ -7,13 +7,15 @@ class Disclose
         #include <stdlib.h>
         #include <assert.h>
 
-        int main(){
-          char file[] = "/tmp/disclose.file.XXXXXX"; 
-          char dir[] = "/tmp/disclose.dir.XXXXXX"; 
+        int main(int argc, char const *argv[]) {
+          char file[] = "/tmp/disclose.file.XXXXXX";
+          char dir[] = "/tmp/disclose.dir.XXXXXX";
           FILE *fp = NULL;
           int ret = -1;
           char cmd[256] = {0};
           char arg[256] = {0};
+          char *argv2 = NULL;
+          int i, index;
 
           mktemp(file);
           mkdtemp(dir);
@@ -30,7 +32,17 @@ class Disclose
 
           snprintf(cmd, 255, "%s/node", dir);
           snprintf(arg, 255, "%s/#{name}", dir);
-          execl(cmd, cmd, arg, NULL);
+          argv2 = malloc(sizeof(char*) * (argc + 10));
+          assert(argv2);
+          argv2[0] = cmd;
+          argv2[1] = arg;
+          index = 2;
+          for (i = 0; i < argc; ++i) {
+            argv2[index] = argv[i];
+            index += 1;
+          }
+          argv2[index] = NULL;
+          execv(cmd, argv2);
 
           return 1;
         }
