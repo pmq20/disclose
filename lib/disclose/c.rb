048498581f785d2f987ff0c7758dc714a6fd7fa8
Minqi Pan <pmq2001@gmail.com>
2016-07-07 16:53:01 +0800

    Different tar wrt. windows

--- lib/disclose/c.rb
+++ lib/disclose/c.rb
@@ -1,52 +1,68 @@
 class Disclose
   module C
-    def self.src(name)
-      %Q{
-        #include <unistd.h>
-        #include <stdio.h>
-        #include <stdlib.h>
-        #include <assert.h>
+    class << self
+      def src(name)
+        %Q{
+          #include <unistd.h>
+          #include <stdio.h>
+          #include <stdlib.h>
+          #include <assert.h>
 
-        int main(int argc, char const *argv[]) {
-          char file[] = "/tmp/disclose.file.XXXXXX";
-          char dir[] = "/tmp/disclose.dir.XXXXXX";
-          FILE *fp = NULL;
-          int ret = -1;
-          char cmd[256] = {0};
-          char arg[256] = {0};
-          char **argv2 = NULL;
-          int i, index;
+          int main(int argc, char const *argv[]) {
+            char file[] = "/tmp/disclose.file.XXXXXX";
+            char dir[] = "/tmp/disclose.dir.XXXXXX";
+            FILE *fp = NULL;
+            int ret = -1;
+            char cmd[256] = {0};
+            char arg[256] = {0};
+            char **argv2 = NULL;
+            int i, index;
 
-          mktemp(file);
-          mkdtemp(dir);
+            mktemp(file);
+            mkdtemp(dir);
 
-          fp = fopen(file, "wb");
-          assert(fp);
+            fp = fopen(file, "wb");
+            assert(fp);
 
-          fwrite(tar_tar, sizeof(unsigned char), sizeof(tar_tar), fp);
-          fclose(fp);
+            fwrite(tar_tar, sizeof(unsigned char), sizeof(tar_tar), fp);
+            fclose(fp);
 
+            #{Gem.win_platform? ? tar_windows : tar}
+
+            snprintf(cmd, 255, "%s/node", dir);
+            snprintf(arg, 255, "%s/#{name}", dir);
+            argv2 = malloc(sizeof(char*) * (argc + 10));
+            assert(argv2);
+            argv2[0] = cmd;
+            argv2[1] = arg;
+            index = 2;
+            for (i = 1; i < argc; ++i) {
+              argv2[index] = argv[i];
+              index += 1;
+            }
+            argv2[index] = NULL;
+            execv(cmd, argv2);
+
+            return 1;
+          }
+        }
+      end
+      
+      def tar
+        %Q{
           snprintf(cmd, 255, "tar xf %s -C %s", file, dir);
           ret = system(cmd);
           assert(0 == ret);
-
-          snprintf(cmd, 255, "%s/node", dir);
-          snprintf(arg, 255, "%s/#{name}", dir);
-          argv2 = malloc(sizeof(char*) * (argc + 10));
-          assert(argv2);
-          argv2[0] = cmd;
-          argv2[1] = arg;
-          index = 2;
-          for (i = 1; i < argc; ++i) {
-            argv2[index] = argv[i];
-            index += 1;
-          }
-          argv2[index] = NULL;
-          execv(cmd, argv2);
-
-          return 1;
         }
-      }
+      end
+      
+      def tar_windows
+        %Q{
+          snprintf(cmd, 255, "tar xf %s -C %s", file, dir);
+          ret = system(cmd);
+          assert(0 == ret);
+        }
+      end
     end
   end
 end
