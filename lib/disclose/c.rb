fe036dd353a7d6375068a0d4337648c79ba8b388
Minqi Pan <pmq2001@gmail.com>
2016-07-28 13:00:31 +0800

    workaround buggy Cygwin execv

--- lib/disclose/c.rb
+++ lib/disclose/c.rb
@@ -8,6 +8,10 @@ class Disclose
             #include <io.h>
             #include <process.h> /* for getpid() and the exec..() family */
             #include <direct.h> /* for _getcwd() and _chdir() */
+
+            #include <windows.h>
+            #include <stdio.h>
+            #include <tchar.h>
           }
         else
           %q{
@@ -89,11 +93,8 @@ class Disclose
           }
 
           int main(int argc, char const *argv[]) {
-            char **argv2 = NULL;
             int i, index;
             struct stat info;
-            char arg0[1024] = {0};
-            char arg1[1024] = {0};
 
             getcwd(cwd_path, sizeof(cwd_path));
             get_tmp_prefix();
@@ -104,24 +105,83 @@ class Disclose
 
             assert(0 == stat( md5_path, &info ) && info.st_mode & S_IFDIR);
 
-            argv2 = malloc(sizeof(char*) * (argc + 10));
-            assert(argv2);
-
-            snprintf(arg0, 1023, "%s#{slash}node", md5_path);
-            snprintf(arg1, 1023, "%s#{slash}#{name}", md5_path);
-
-            argv2[0] = arg0;
-            argv2[1] = arg1;
-            index = 2;
-            for (i = 1; i < argc; ++i) {
-              argv2[index] = argv[i];
-              index += 1;
-            }
-            argv2[index] = NULL;
-            execv(argv2[0], argv2);
-
-            return 1;
+            #{Gem.win_platform? ? execute_unix : execute_windows}
           }
+        }
+      end
+      
+      def execute_unix
+        %Q{
+          char **argv2 = NULL;
+          char arg0[1024] = {0};
+          char arg1[1024] = {0};
+
+          argv2 = malloc(sizeof(char*) * (argc + 10));
+          assert(argv2);
+
+          snprintf(arg0, 1023, "%s#{slash}node", md5_path);
+          snprintf(arg1, 1023, "%s#{slash}#{name}", md5_path);
+
+          argv2[0] = arg0;
+          argv2[1] = arg1;
+          index = 2;
+          for (i = 1; i < argc; ++i) {
+            argv2[index] = argv[i];
+            index += 1;
+          }
+          argv2[index] = NULL;
+
+          execv(argv2[0], argv2);
+
+          return 1;
+        }
+      end
+      
+      def execute_windows
+        %Q{
+          char arg0[32768] = {0};
+
+          char *arg0_ptr = arg0;
+          snprintf(arg0_ptr, 32767 - strlen(arg0), "%s#{slash}node", md5_path);
+          arg0_ptr += strlen(arg0_ptr);
+          snprintf(arg0_ptr, 32767 - strlen(arg0), " \\"%s#{slash}#{name}\\" ", md5_path);
+          for (i = 1; i < argc; ++i) {
+            arg0_ptr += strlen(arg0_ptr);
+            snprintf(arg0_ptr, 32767 - strlen(arg0), " \\"%s\\" ", argv[i]);
+          }
+
+          STARTUPINFO si;
+          PROCESS_INFORMATION pi;
+
+          ZeroMemory( &si, sizeof(si) );
+          si.cb = sizeof(si);
+          ZeroMemory( &pi, sizeof(pi) );
+
+          // Start the child process.
+          if( !CreateProcess( NULL,   // No module name (use command line)
+              arg0,           // Command line
+              NULL,           // Process handle not inheritable
+              NULL,           // Thread handle not inheritable
+              FALSE,          // Set handle inheritance to FALSE
+              0,              // No creation flags
+              NULL,           // Use parent's environment block
+              NULL,           // Use parent's starting directory 
+              &si,            // Pointer to STARTUPINFO structure
+              &pi )           // Pointer to PROCESS_INFORMATION structure
+          ) 
+          {
+              printf( "CreateProcess failed (%d).\n", GetLastError() );
+              return 101;
+          }
+
+          // Wait until child process exits.
+          WaitForSingleObject( pi.hProcess, INFINITE );
+
+          // Close process and thread handles. 
+          CloseHandle( pi.hProcess );
+          CloseHandle( pi.hThread );
+
+          return 0;
         }
       end
 
